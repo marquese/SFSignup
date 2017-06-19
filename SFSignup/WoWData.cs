4e8847919db7b22d6f7855c921e2832423985779
Marcus White <mwhite@radiantworlds.com>
2017-06-19 10:10:55 +0100

    Cleaned up namespaces on GetClassColour

--- SFSignup/WoWData.cs
+++ SFSignup/WoWData.cs
@@ -4,17 +4,17 @@ namespace SFSignup.WoW
 {
     public class ClassColours
     {
-        public static Color Warrior { get { return Color.FromRgb(199, 156, 110); } }
-        public static Color Paladin { get { return Color.FromRgb(245, 140, 186); } }
-        public static Color Hunter { get { return Color.FromRgb(171, 212, 115); } }
-        public static Color Rogue { get { return Color.FromRgb(255, 245, 105); } }
-        public static Color Priest { get { return Color.FromRgb(255, 255, 255); } }
+        public static Color Warrior     { get { return Color.FromRgb(199, 156, 110); } }
+        public static Color Paladin     { get { return Color.FromRgb(245, 140, 186); } }
+        public static Color Hunter      { get { return Color.FromRgb(171, 212, 115); } }
+        public static Color Rogue       { get { return Color.FromRgb(255, 245, 105); } }
+        public static Color Priest      { get { return Color.FromRgb(255, 255, 255); } }
         public static Color DeathKnight { get { return Color.FromRgb(196, 30, 59); } }
-        public static Color Shaman { get { return Color.FromRgb(0, 112, 222); } }
-        public static Color Mage { get { return Color.FromRgb(105, 204, 240); } }
-        public static Color Warlock { get { return Color.FromRgb(148, 130, 201); } }
-        public static Color Monk { get { return Color.FromRgb(0, 255, 150); } }
-        public static Color Druid { get { return Color.FromRgb(255, 125, 10); } }
+        public static Color Shaman      { get { return Color.FromRgb(0, 112, 222); } }
+        public static Color Mage        { get { return Color.FromRgb(105, 204, 240); } }
+        public static Color Warlock     { get { return Color.FromRgb(148, 130, 201); } }
+        public static Color Monk        { get { return Color.FromRgb(0, 255, 150); } }
+        public static Color Druid       { get { return Color.FromRgb(255, 125, 10); } }
         public static Color DemonHunter { get { return Color.FromRgb(163, 48, 201); } }
 
         public static Color GetClassColour(PlayerClass c)
@@ -22,40 +22,40 @@ namespace SFSignup.WoW
             switch (c)
             {
                 case PlayerClass.DeathKnight:
-                    return ClassColours.DeathKnight;
+                    return DeathKnight;
 
                 case PlayerClass.DemonHunter:
-                    return ClassColours.DemonHunter;
+                    return DemonHunter;
 
                 case PlayerClass.Druid:
-                    return ClassColours.Druid;
+                    return Druid;
 
                 case PlayerClass.Hunter:
-                    return ClassColours.Hunter;
+                    return Hunter;
 
                 case PlayerClass.Mage:
-                    return ClassColours.Mage;
+                    return Mage;
 
                 case PlayerClass.Monk:
-                    return ClassColours.Monk;
+                    return Monk;
 
                 case PlayerClass.Paladin:
-                    return ClassColours.Paladin;
+                    return Paladin;
 
                 case PlayerClass.Priest:
-                    return ClassColours.Priest;
+                    return Priest;
 
                 case PlayerClass.Rogue:
-                    return ClassColours.Rogue;
+                    return Rogue;
 
                 case PlayerClass.Shaman:
-                    return ClassColours.Shaman;
+                    return Shaman;
 
                 case PlayerClass.Warlock:
-                    return ClassColours.Warlock;
+                    return Warlock;
 
                 case PlayerClass.Warrior:
-                    return ClassColours.Warrior;
+                    return Warrior;
             }
             return DeathKnight;
         }
