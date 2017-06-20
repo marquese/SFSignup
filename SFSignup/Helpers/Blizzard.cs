64c982e7882d08f87a9426b6877364c624cd7fcc
Marq <marquese@maiev.net>
2017-06-20 22:51:57 +0100

    Added helper classes for interacting with MongoDB & the Blizzard API

---
+++ SFSignup/Helpers/Blizzard.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SFSignup
+{
+    public sealed class Blizzard
+    {
+        public static async Task<List<RaidZone>> GetRaidZones()
+        {
+            var datastring = await RequestAsync("zone");
+            var data = from x in JObject.Parse(datastring)["zones"]
+                       where x["isRaid"].ToObject<bool>() == true
+                       select x;
+
+            // Get only the most recent raid zones
+            var highestExpansionID = (from x in data
+                                      group x by x["expansionId"].ToObject<int>() into g
+                                      select g.Key).OrderBy(y => y).Last();
+
+            var outputData = (from x in data
+                              where x["expansionId"].ToObject<int>() == highestExpansionID
+                              select x.ToObject<RaidZone>()).ToList();
+
+            return outputData;
+        }
+
+        private static string Request(string url)
+        {
+            var u = "https://eu.api.battle.net/wow/" + url + "/?locale=en_GB&apikey=" + Settings.Blizzard.APIKey;
+            return new WebClient().DownloadString(u);
+        }
+
+        private static async Task<string> RequestAsync(string url)
+        {
+            var u = "https://eu.api.battle.net/wow/" + url + "/?locale=en_GB&apikey=" + Settings.Blizzard.APIKey;
+            var result = await new WebClient().DownloadStringTaskAsync(new Uri(u));
+            return result;
+        }
+    }
+}
---
+++ SFSignup/Helpers/MongoDatabase.cs
@@ -0,0 +1,83 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFSignup
+{
+    public sealed class MongoDatabase
+    {
+        private static MongoClient Client;
+        private static IMongoDatabase Database;
+
+        public static async Task AddRaiderAsync(Raider raider)
+        {
+            var db = Database.GetCollection<Raider>(Settings.Mongo.Collections.Raiders);
+            var existing = await db.FindAsync(new FilterDefinitionBuilder<Raider>().Eq("Name", raider.Name));
+            if (existing.Any())
+            {
+                Console.WriteLine($"Can't add {raider.Name}. A raider with that name already exists");
+                return;
+            }
+
+            await db.InsertOneAsync(raider);
+        }
+
+        internal static async Task AddEventAsync(Event newEvent)
+        {
+            var db = Database.GetCollection<Event>(Settings.Mongo.Collections.Events);
+
+            await db.InsertOneAsync(newEvent);
+        }
+
+        public static async Task<IEnumerable<Event>> GetEventsAsync()
+        {
+            var result = await Database.GetCollection<Event>(Settings.Mongo.Collections.Events).FindAsync(FilterDefinition<Event>.Empty);
+            return result.ToList();
+        }
+
+        public static void Connect()
+        {
+            Client = new MongoClient(Settings.Mongo.URL);
+            Database = Client.GetDatabase(Settings.Mongo.DatabaseName);
+            Console.WriteLine("Connected to database.");
+
+            return;
+
+            var desiredCollections = typeof(Settings.Mongo.Collections).GetProperties().Select(x => (string)x.GetValue(null));
+
+            
+            foreach (var collection in desiredCollections)
+            {
+                CollectionExistsAsync(collection).ContinueWith(x =>
+                {
+                    if (!x.Result)
+                    {
+                        Database.CreateCollectionAsync(collection);
+                        Console.WriteLine($"Created collection: {collection}");
+                    }
+                }).Wait();
+            }
+        }
+
+        private static async Task<bool> CollectionExistsAsync(string collectionName)
+        {
+            var filter = new BsonDocument("name", collectionName);
+            var collections = await Database.ListCollectionsAsync(new ListCollectionsOptions { Filter = filter });
+            return await collections.AnyAsync();
+        }
+
+        public static async Task<IEnumerable<Raider>> GetRaidersAsync()
+        {
+            var result = await Database.GetCollection<Raider>(Settings.Mongo.Collections.Raiders).FindAsync(FilterDefinition<Raider>.Empty);
+            return result.ToEnumerable();
+        }
+
+        public static IEnumerable<Raider> GetRaiders()
+        {
+            return Database.GetCollection<Raider>(Settings.Mongo.Collections.Raiders).Find(FilterDefinition<Raider>.Empty).ToEnumerable();
+        }
+    }
+}
