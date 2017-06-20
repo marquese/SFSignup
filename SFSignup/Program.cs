dc7d94379617bd12795d103e9563e799cdf030d9
Marq <marquese@maiev.net>
2017-06-20 22:54:28 +0100

    Some (Messy) examples of interaction

--- SFSignup/Program.cs
+++ SFSignup/Program.cs
@@ -1,14 +1,33 @@
 ﻿using Nancy.Hosting.Self;
 using System;
+using System.Collections.Generic;
 
 namespace SFSignup
 {
     internal class Program
     {
+
+        public static List<RaidZone> RaidZones;
         private static void Main(string[] args)
         {
-            var url = "http://127.0.0.1:9000";
+            //Blizzard.GetRaidZones().ContinueWith((t) => { RaidZones = t.Result; });
 
+            Settings.Load();
+            var url = "http://127.0.0.1:9000";
+            MongoDatabase.Connect();
+
+            //MongoDatabase.AddRaiderAsync(new Raider() { Name = "Iluin" }).Wait();
+            //MongoDatabase.AddEventAsync(new Event() { Name = "Iluin",Location=RaidZones[0],StartTime=DateTime.Now,EndTime=DateTime.Now + new TimeSpan(1,0,0) }).Wait();
+
+            //var eventsTask = MongoDatabase.GetEventsAsync();
+            //eventsTask.Wait();
+
+            //var events = eventsTask.Result;
+            MongoDatabase.GetRaidersAsync().ContinueWith((t) =>
+            {
+                foreach (var result in t.Result)
+                    Console.WriteLine(result.Name);
+            });
             using (var host = new NancyHost(new Uri(url)))
             {
                 host.Start();
