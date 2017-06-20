3c12bae1e8ba0071dcfb2b6a40f392daddc42d64
Marq <marquese@maiev.net>
2017-06-20 22:50:11 +0100

    Added a default module/view for NancyFX

---
+++ SFSignup/HomeModule.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFSignup.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            this.Get["/"] = _ => new HomeModel("Hello world");
+        }
+
+        private class HomeModel
+        {
+            public HomeModel(string text)
+            {
+                this.Text = text;
+            }
+
+            public string Text { get; }
+
+            public string Title => "Home Page";
+        }
+    }
+}
