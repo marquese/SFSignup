b8801400570bc577c19fd889a66cad4c78c5d7e9
Marq <marquese@maiev.net>
2017-06-22 22:49:58 +0100

    Formatted Start/End time for raids to use UTC Epoch ms

--- SFSignup/Event.cs
+++ SFSignup/Event.cs
@@ -1,7 +1,12 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace SFSignup
 {
@@ -12,11 +17,13 @@ namespace SFSignup
         }
 
         [BsonId]
+        [JsonIgnore]
         public ObjectId ID { get; set; }
 
         public string Name { get; set; }
 
         [BsonIgnore]
+        [JsonIgnore]
         public RaidZone Location { get; set; }
 
         public int LocationID
@@ -31,7 +38,75 @@ namespace SFSignup
             }
         }
 
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        [BsonSerializer(typeof(RaiderListSerializer))]
+        public List<Raider> DPS = new List<Raider>();
+        [BsonSerializer(typeof(RaiderListSerializer))]
+        public List<Raider> Healers = new List<Raider>();
+        [BsonSerializer(typeof(RaiderListSerializer))]
+        public List<Raider> Tanks = new List<Raider>();
+        [BsonSerializer(typeof(RaiderListSerializer))]
+        public List<Raider> Unavailable = new List<Raider>();
+
+        [BsonIgnore]
+        [JsonIgnore]
+        public DateTime StartTime
+        {
+            get
+            {
+                return DateTimeOffset.FromUnixTimeSeconds(Start).DateTime;
+            }
+            set
+            {
+                Start = new DateTimeOffset(value.ToUniversalTime()).ToUnixTimeMilliseconds();
+            }
+        }
+        [BsonIgnore]
+        [JsonIgnore]
+        public DateTime EndTime
+        {
+            get
+            {
+                return DateTimeOffset.FromUnixTimeSeconds(End).DateTime;
+            }
+            set
+            {
+                End = new DateTimeOffset(value.ToUniversalTime()).ToUnixTimeMilliseconds();
+            }
+        }
+
+        private string logUrl;
+
+        public string ReportID
+        {
+            get
+            {
+                if (logUrl == null)
+                {
+                    var url = @"https://www.warcraftlogs.com:443/v1/reports/guild/Seventh%20Flight/Argent-Dawn/EU?start=" + Start.ToString() + "&end=" + End.ToString() + "&api_key=" + Settings.WarcraftLogs.Key;
+                    var dataString = new WebClient().DownloadString(url);
+                    var data = JArray.Parse(dataString);
+                    if (data.Count == 0)
+                        logUrl = null;
+                    else
+                    {
+                        logUrl = data.Last["id"].ToString();
+                        SaveToDatabase();
+                    }
+                }
+                return logUrl;
+            }
+            set
+            {
+                logUrl = value;
+            }
+        }
+
+        public void SaveToDatabase()
+        {
+            MongoDatabase.UpdateEvent(this);
+        }
+
+        public long Start { get; set; }
+        public long End { get; set; }
     }
 }
